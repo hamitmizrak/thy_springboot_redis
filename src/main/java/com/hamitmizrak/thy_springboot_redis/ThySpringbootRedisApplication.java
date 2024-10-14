e2c1a378963fc50c6be877f88737a0a621861f8d
Hamit Mizrak <hamitmizrak@gmail.com>
2024-10-14 10:37:07 +0300

    Spring Boot init

---
+++ src/main/java/com/hamitmizrak/thy_springboot_redis/ThySpringbootRedisApplication.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.thy_springboot_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThySpringbootRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThySpringbootRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamitmizrak/thy_springboot_redis/ThySpringbootRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.thy_springboot_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThySpringbootRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
