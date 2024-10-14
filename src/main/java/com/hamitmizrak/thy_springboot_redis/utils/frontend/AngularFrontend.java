ec1b1d8c2968a3705f73423a7195c7e047a835f3
Hamit Mizrak <hamitmizrak@gmail.com>
2024-10-14 15:37:14 +0300

    AngularFrontend

---
+++ src/main/java/com/hamitmizrak/thy_springboot_redis/utils/frontend/AngularFrontend.java
@@ -0,0 +1,6 @@
+package com.hamitmizrak.thy_springboot_redis.utils.frontend;
+
+public class AngularFrontend {
+    // Const
+    public static final String ANGULAR_FRONTEND="http://localhost:1111";
+}
