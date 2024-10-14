bd90c672c4b5f0846e0d948e8aff907faddb69c7
Hamit Mizrak <hamitmizrak@gmail.com>
2024-10-14 11:57:34 +0300

    @SpringBootApplication

--- src/main/java/com/hamitmizrak/thy_springboot_redis/ThySpringbootRedisApplication.java
+++ src/main/java/com/hamitmizrak/thy_springboot_redis/ThySpringbootRedisApplication.java
@@ -3,11 +3,63 @@ package com.hamitmizrak.thy_springboot_redis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// Mongo aktif etmek ici
+// @EnableMongoRepositories
+
+// Aspect aktif etmek icin
+// @EnableAspectJAutoProxy(proxyTargetClass = true)
+
+// Asenkron açmak icin
+// @EnableAsync
+
+// SCAN
+//@EntityScan(basePackages = "com.hamitmizrak.thy_springboot_redis.data.entity") //Entity bulamadığı zaman
+//@EnableJpaRepositories(basePackages = "com.hamitmizrak.thy_springboot_redis.data.repository") //Repository bulamadığı zaman
+//@ComponentScan("com")
+
+// Spring Cache aktif etmek gerekiyor.
+// @EnableCaching
+
+// Auditing Aktif etmek
+// @EnableJpaAuditing(auditorAwareRef = "auditorAwareBeanMethod")
+
+// Spring Security: Şimdilik dahil etme, çünkü Bcrypted kullancağım ancak Spring security için gerekli kütüphaneleri dahil
+// Buradaki exclude ne zaman kapatmam gerekiyor ? cevap: Spring Security ile çalıştığımız zaman kapat
+@SpringBootApplication(exclude = {
+        //SecurityAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+  }
+)
+
+//@SpringBootApplication
 public class ThySpringbootRedisApplication {
+	 /*
+		 @PostConstruct
+			public void init() {
+				TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+			}
+    */
 
+	// PSVM
 	public static void main(String[] args) {
-		SpringApplication.run(ThySpringbootRedisApplication.class, args);
-	}
 
-}
+		// devtools active isActive
+		// System.setProperty("spring.devtools.restart.enabled","true");
+
+		// PORT Ayarlamak
+        /*
+        SpringApplication app = new SpringApplication(ThySpringbootRedisApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "4444"));
+        app.run(args);
+         */
+
+		// JOptional pane aktif etmek
+		System.setProperty("java.awt.headless", "false");
+
+		// Main
+		SpringApplication.run(ThySpringbootRedisApplication.class, args);
+	} //end psvm
+
+} //end class ThySpringbootRedisApplication
