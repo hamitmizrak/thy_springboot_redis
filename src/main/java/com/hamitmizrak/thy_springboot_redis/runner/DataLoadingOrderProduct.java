947dcdab730c21ff8d83448ff3efcd2e584141ea
Hamit Mizrak <hamitmizrak@gmail.com>
2024-10-17 10:25:00 +0300

    AddressQrCode I18N revize yaptım

---
+++ src/main/java/com/hamitmizrak/thy_springboot_redis/runner/DataLoadingOrderProduct.java
@@ -0,0 +1,23 @@
+package com.hamitmizrak.thy_springboot_redis.runner;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+// @Component : Spring Framework'un bir anatasyonudur. Spring Frameworkun bir bileşen sınıfı olduğunu gösteriyor
+// Spring'in otomatik bu sınıfı bir bean nesnesi olarak tanımasına izin veriyoruz.
+
+// LOMBOK
+@RequiredArgsConstructor
+@Log4j2
+
+@Component
+public class DataLoadingOrderProduct implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Order Product");
+        log.info("Order Product");
+    }
+}
