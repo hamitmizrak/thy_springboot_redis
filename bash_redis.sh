61f68123723e785103b58b316d21641d9d4fa4e3
Hamit Mizrak <hamitmizrak@gmail.com>
2024-10-18 15:32:12 +0300

    Redis Examples

--- bash_redis.sh
+++ bash_redis.sh
@@ -3,8 +3,18 @@
 
 # Projemizin sağlıklı çalışması için öncelikler redis servera kurulu olması gerekiyor
 docker container run --name spring_redis -p 6379:6379 -d redis
-# docker exec -it spring_redis
+
+# Redis Servera bağlanmak için
+# 1.YOL: Docker Desktop => Container => Terminal
+# 2.YOL: Terminal       => winpty docker exec -it spring_redis bash
+
+# docker exec -it spring_redis bash
 # docker exec -it spring_redis redis-cli
+
+# $ redis-cli ping     NOT: Eğer bu ping sonucu PONG geliyorsa redis server çalışıyor demektir.
+# $ redis-cli FLUSHALL
+# $ redis-cli FLUSHDB
+# $ redis-cli DEL id   nOT: id: cache id bileşenidir.
 
 # Spring Boot projesini çalıştır.
 mvn spring-boot:run
