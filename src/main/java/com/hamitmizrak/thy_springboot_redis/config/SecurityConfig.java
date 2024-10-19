db595999f7a853b43af60b1bd779d458e6ac238d
Hamit Mizrak <hamitmizrak@gmail.com>
2024-10-19 14:06:05 +0300

    Security revize

--- src/main/java/com/hamitmizrak/thy_springboot_redis/config/SecurityConfig.java
+++ src/main/java/com/hamitmizrak/thy_springboot_redis/config/SecurityConfig.java
@@ -140,3 +140,41 @@ public class SecurityConfig {
 } // end SecurityFilterChain
 
 
+// 1.ADIM
+// Spring Security: Şimdilik dahil etme, çünkü Bcrypted kullancağım ancak Spring security için gerekli kütüphaneleri dahil
+// Buradaki exclude ne zaman kapatmam gerekiyor ? cevap: Spring Security ile çalıştığımız zaman kapat
+/*@SpringBootApplication(exclude = {
+        //SecurityAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)*/
+
+// Spring Security kullandığım zaman => @SpringBootApplication
+
+
+// 2.ADIM
+/*devtools: otomatik sistemi compiler etmek içindir-->
+        <!--devtools: ancak Spring security kapatabilirsin-->
+        <!-- <dependency>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-devtools</artifactId>
+        <optional>true</optional>
+        <scope>runtime</scope>
+        </dependency>
+  */
+
+
+// 3.ADIM (Default =>Login )
+// Username: user
+// Password: 9be827ae-4d3b-4b0f-b37a-a4666e1ce59f
+
+// 4.ADIM (application.properties)
+/*
+### Spring Security  #################################################################
+spring.command.line.rules.superadmin.nickname=superadmin
+spring.command.line.rules.superadmin.name=Hamit
+spring.command.line.rules.superadmin.surname=Mızrak
+spring.command.line.rules.superadmin.email=hamitmizrak@gmail.com
+spring.command.line.rules.superadmin.password=root
+spring.command.line.rules.superadmin.roles=SUPER_ADMIN
+*/
